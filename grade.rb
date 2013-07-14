831d50365991dbcca316e54dd1f7483834b0ac0c
Elina Desaine <edesaine@ticklo.com>
2013-07-14 19:42:51 +0100

    Grade file passes

--- grade.rb
+++ grade.rb
@@ -4,11 +4,27 @@
 #  40 - 60   => 'c'
 #  20 - 40   => 'd'
 #  0  - 20   => 'e'
-def grade(mark)
-	## TODO write your code here. You might have to use multiple 
-	# if statements inside each other	
 
-	return grade
+
+
+def grade (mark)
+
+    if  mark >=80
+        grade =  'a'
+    end
+    if mark <80 && mark>=60
+        grade ='b'
+    end
+    if mark <60 && mark >=40 
+        grade ='c'
+    end
+     if mark <40 && mark >=30 
+        grade = 'd'
+    end
+     if mark <20
+        grade ='e'
+    end
+
+    return grade
 end
-
 
