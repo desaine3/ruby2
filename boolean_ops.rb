b07c7c1e85f095f8abd9763f58fb3c61ca721007
Elina Desaine <edesaine@ticklo.com>
2013-07-17 00:08:38 +0100

    Passes test

--- boolean_ops.rb
+++ boolean_ops.rb
@@ -1,9 +1,10 @@
 
-1 > 2 && 3 > 4
+1 > 2 && 3 > 4 #False
 
-1==1 || 10 ==9
+1==1 || 10 ==9 # True
 
-!(5<6)
+!(5<6) #False
 
-(1<=2 && 3>4) || (3 <= 4 && "ham")
+(1<=2 && 3>4) || (3 <= 4 && "ham") #"ham" Most objects are true. 
+#'FALSE' & 'Nil' are the only falsey
 
--- bouncer.rb
+++ bouncer.rb
@@ -11,3 +11,19 @@
 #     Welcome to the club, Doris!
 
 # TODO: Write this program! (You have to be 18 to enter the club.)
+
+
+
+    print "What's your name?"
+    name= gets.chomp.capitalize
+
+    print "How old are you, " + name +"? "
+    age = gets.chomp.to_i
+
+
+    if age >=18 
+        print "Welcome to the club, " + name + "!"
+
+    else
+        print "I'm afraid I can't let you in, " + name + ". You're too young!"
+end
